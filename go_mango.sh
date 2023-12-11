94bebdbd7cc2f32d48f125e34ee479c1ff4fe811
Christian Zufferey <christian@zufferey.com>
2023-12-11 14:37:01 +0100

    new: ajouté un petit script pour charger facilement mes alias sur Mango

---
+++ go_mango.sh
@@ -0,0 +1,16 @@
+#!/bin/ash
+#Petit script pour pour charger les alias sur le Mango ;-)
+#zf231211.1413
+
+echo -e "
+
+Faire
+
+source ./go_mango.sh
+
+"
+
+
+source /mnt/sda1/mango/alias
+cd /mnt/sda1/zuzu/esp32-cam-st-luc/
+
