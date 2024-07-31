af007db24c5fba5adc4016f2855a674d29472d07
Christian Zufferey <christian@zufferey.com>
2024-07-31 15:57:40 +0200

    change: changé de web serveur afin de pouvoir afficher le détail du directory browsing

--- start_web_server.sh
+++ start_web_server.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
-#Petit script pour démarrer un mini serveur WEB
-#zf231211.1756
+
+# Petit script pour démarrer un mini serveur WEB, attention cela n'utilise plus python mais lighttpd !
+#
+# ATTENTION: il faut modifier le port et le dossier racine dans le fichier $zAPP_FOLDER/lighttpd.conf !
+#
+#zf231211.1756, zf240731.1556
+
 
 zAPP_FOLDER=$(/usr/bin/dirname $0)
 zDATA_FOLDER=/home/ubuntu/data
 
-#permet de rendre non fonctionnel le directory browsing dans la racine
-#afin de diminuer la surface d'attaque
+
+# permet de rendre non fonctionnel le directory browsing dans la racine
+# afin de diminuer la surface d'attaque
 touch $zDATA_FOLDER/index.html
 
-while true ; do
-    pushd $zDATA_FOLDER; python3 -m http.server 30080; popd
-done
+sudo lighttpd -f $zAPP_FOLDER/lighttpd.conf
 
 
 echo -e "
@@ -21,6 +25,17 @@ Si jamais pour info:
 crontab -e
 @reboot  sleep 30 && /home/ubuntu/dev/esp32-cam-st-luc/start_web_server.sh
 
+Et pour l'arrêter facilement il faut faire: 
+
+sudo pkill lighttpd
+
 "
 
+
+exit
+
+
+while true ; do
+    pushd $zDATA_FOLDER; python3 -m http.server 30080; popd
+done
 
