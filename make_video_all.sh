9c3126d480f1e23422731cf8543d19f31339c1fc
Christian Zufferey <christian@zufferey.com>
2024-10-24 00:06:01 +0200

    new: voilà la vidéo de la caméra de la cuisine fonctionne aussi maintenant \o/

--- make_video_all.sh
+++ make_video_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Petit script pour la réalisation d'un petit film à base d'images pour la caméra de St-Luc
-# zf231209.1736, zf231211.2055
+# zf231209.1736, zf231211.2055, zf241023.2316
 #
 # Sources: https://www.rickmakes.com/create-timelapse-from-ip-camera-using-curl-wget-and-ffmpeg/
 
@@ -26,10 +26,18 @@ zACTUAL_FOLDER: $zACTUAL_FOLDER
 "
 
 
-mkdir -p $zTARGET_VIDEOS
-rm $zTARGET_VIDEOS/0_video.mp4
-$zAPP_FOLDER/make_video_one.sh $zTARGET_IMAGES $zTARGET_VIDEOS
-cp $zTARGET_VIDEOS/0_video.mp4 $zACTUAL_FOLDER/actual.mp4
+# Caméra externe
+mkdir -p $zTARGET_VIDEOS/externe
+rm $zTARGET_VIDEOS/externe/0_video.mp4
+$zAPP_FOLDER/make_video_one.sh $zTARGET_IMAGES/externe $zTARGET_VIDEOS/externe
+cp $zTARGET_VIDEOS/externe/0_video.mp4 $zACTUAL_FOLDER/externe.mp4
+
+
+# Caméra cuisine
+mkdir -p $zTARGET_VIDEOS/cuisine
+rm $zTARGET_VIDEOS/cuisine/0_video.mp4
+$zAPP_FOLDER/make_video_one.sh $zTARGET_IMAGES/cuisine $zTARGET_VIDEOS/cuisine
+cp $zTARGET_VIDEOS/cuisine/0_video.mp4 $zACTUAL_FOLDER/cuisine.mp4
 
 
 
